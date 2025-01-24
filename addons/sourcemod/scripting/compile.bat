12f68708da06ab3156996571f8fc674b72f59c8e
ficool2 <34815548+ficool2@users.noreply.github.com>
2025-01-24 15:24:35 +0000

    Commit plugin to repo
    Move compile script out to tools folder
    This is for easier updating

--- addons/sourcemod/scripting/compile.bat
+++
@@ -1,3 +0,0 @@
-"../../../../../addons/sourcemod/scripting/spcomp.exe" tf2ware_ultimate.sp
-move tf2ware_ultimate.smx "../../../../../addons/sourcemod/plugins/"
-@pause
---
+++ tools/compile_plugin.bat
@@ -0,0 +1,5 @@
+cd "../addons/sourcemod/scripting"
+"../../../../../addons/sourcemod/scripting/spcomp.exe" tf2ware_ultimate.sp
+copy tf2ware_ultimate.smx "../../../../../addons/sourcemod/plugins/"
+move tf2ware_ultimate.smx "../plugins/"
+@pause
